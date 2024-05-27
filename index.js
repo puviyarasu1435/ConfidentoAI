18e19a584ccdbbf42a691aa3f79a1d972634520a
puviyarasu P <puviyarasu5341@gmail.com>
2024-05-27 16:49:27 +0530

    Gemini

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const GeminiAIRoutes = require('./routes/Gemini');
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/submit', (req, res) => {
     res.send("hellow");
 });
-// Start the server
+
+
+app.use('/AI', GeminiAIRoutes);
+
+
+
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
