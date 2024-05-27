cdd2687a6f392ffe1256d295f84cf4b2b3c36c9c
puviyarasu P <puviyarasu5341@gmail.com>
2024-05-27 12:47:44 +0530

    Test

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/submit', (req, res) => {
+    res.send("hellow");
+});
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
