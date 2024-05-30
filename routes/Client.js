17b3c35078c57c99fc87f25f0ef0b790fa5628cc
puviyarasu P <puviyarasu5341@gmail.com>
2024-05-30 10:30:32 +0530

    env

--- routes/Client.js
+++ routes/Client.js
@@ -1,54 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
-const path = require('path');
-var ActiveAction = "IDEL";
+
+let ActiveAction = process.env.ACTIVE_ACTION || "IDEL"; // Default value if not set in environment variable
 let AudioList = {};
-
-const readData = () => {
-    const filePath = path.join(__dirname, 'data.json');
-    const data = fs.readFileSync(filePath, 'utf8');
-    return JSON.parse(data);
-};
-
-const writeData = (newData) => {
-    const filePath = path.join(__dirname, 'data.json');
-    try {
-        // Read existing data
-        const existingData = readData();
-        // Update existing data with new data
-        const updatedData = { ...existingData, ...newData };
-        // Write updated data back to file
-        fs.writeFileSync(filePath, JSON.stringify(updatedData, null, 2), 'utf8');
-    } catch (error) {
-        console.error('Error writing file:', error.message);
-        throw error;
-    }
-};
 
 router.get('/Action', async (req, res) => {
     try {
-        const data = readData();
-        res.send(data["Action"]);
-        //res.send(ActiveAction);
+        res.send(ActiveAction);
     } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'An error occurred while generating content.' });
+        console.error('Error:', error.message);
+        res.status(500).json({ error: 'An error occurred while retrieving Action data.' });
     }
 });
+
 router.post('/Action', async (req, res) => {
     try {
         const { Action } = req.body;
         if (!Action) {
             return res.status(400).json({ error: 'Invalid request. "Action" is required.' });
         }
-        const newData = { Action };
-        writeData(newData);
-        console.log('Updated data:', newData);
-        res.send(Action);
+        ActiveAction = Action;
+        console.log('Updated Action:', ActiveAction);
+        res.send(ActiveAction);
     } catch (error) {
         console.error('Error:', error.message);
-        res.status(500).json({ error: 'An error occurred while processing your request.' });
+        res.status(500).json({ error: 'An error occurred while processing Action update.' });
     }
 });
 
@@ -56,10 +32,9 @@ router.get('/AudioList', async (req, res) => {
     try {
         res.json(AudioList);
     } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'An error occurred while generating content.' });
+        console.error('Error:', error.message);
+        res.status(500).json({ error: 'An error occurred while retrieving AudioList.' });
     }
 });
-
 
 module.exports = router;
